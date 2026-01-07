1ca64e1cf950982a3c9e275211aa271853be0ea2
Arif Ozberk Azmak <ozberkazmak@gmail.com>
2026-01-07 13:40:13 +0300

    responsive navbar

--- src/components/shared_components/Navbar.jsx
+++ src/components/shared_components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 // Styles
 import styles from "../../styles/sharedComponents_styles/Navbar.module.scss";
@@ -9,16 +9,44 @@ import { Link } from 'react-router';
 // Components
 import Logo from './Logo';
 
+// React-Icons
+import { FaChevronDown } from "react-icons/fa6";
+
 
 const Navbar = () => {
 
+    const [isNavlinksOpen, setIsNavlinksOpen] = useState(false);
+
+    const navbarRef = useRef(null);
+
+
+    useEffect(() => {  // Function that closes the dropdown when user clicks somewhere else
+        const handleClickOutsideNavlinks = (event) => {
+            if (navbarRef.current && !navbarRef.current.contains(event.target)) {
+                setIsNavlinksOpen(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutsideNavlinks);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutsideNavlinks);
+        };
+    }, [setIsNavlinksOpen]);
+
 
     return (
-        <nav className={styles.Navbar}>
+        <nav className={styles.Navbar} ref={navbarRef}>
             <section className={styles.navLeft}>
                 <Logo logoHeight={"3rem"} />
 
-                <ul className={styles.navLinks}>
+                <ul
+                    className={styles.navLinks}
+                    style={{
+                        transform: isNavlinksOpen ? "translateY(4rem)" : "",
+                        opacity: isNavlinksOpen ? "1" : ""
+                    }}
+                >
                     <li><Link to="/" >Anasayfa</Link></li>
                     <li><Link to="/projeler" >Projeler</Link></li>
                     <li><Link to="/referanslar" >Referanslar</Link></li>
@@ -28,7 +56,11 @@ const Navbar = () => {
             </section>
 
             <section className={styles.navRight}>
-
+                <FaChevronDown
+                    className={styles.dropdownButton}
+                    onClick={() => setIsNavlinksOpen(prev => !prev)}
+                    style={{ transform: isNavlinksOpen ? "rotateX(180deg)" : "rotateX(0deg)" }}
+                />
             </section>
         </nav>
     )
